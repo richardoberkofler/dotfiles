0b0b471d64489de5aadc145f5ba28f6a39d33ebb
Richard Oberkofler <102246582+richardoberkofler@users.noreply.github.com>
2025-08-22 11:04:26 +0200

    update ubuntu.sh

--- setup/ubuntu.sh
+++ setup/ubuntu.sh
@@ -15,7 +15,13 @@ fi
 
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install -y git zsh tmux curl unzip build-essential fzf eza zoxide neovim htop ripgrep neofetch stow lazygit cloc
+sudo apt-get install -y git zsh tmux curl unzip build-essential fzf eza zoxide neovim htop ripgrep neofetch stow cloc
+
+# Install lazygit
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit -D -t /usr/local/bin/
 
 # Install starship
 if ! command -v starship &>/dev/null; then
